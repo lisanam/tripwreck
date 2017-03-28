7e2dac608bd7ccf6490af376667cb4186d51373b
Lisa Nam <lsyn2016@gmail.com>
2017-03-27 20:24:03 -0400

    Implemeted pin in Map component

--- tripwreck_mobile/src/components/common/Map.js
+++ tripwreck_mobile/src/components/common/Map.js
@@ -3,19 +3,44 @@ import { View, StyleSheet } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 
-export default class Map extends Component {
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <MapView 
-        style={StyleSheet.absoluteFill}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}/>
-      </View>
-    )
-  }
+const Map = (props) => {
+
+  // componentDidMount() {
+  //   navigator.geolocation.getCurrentPosition(
+  //     (position) => {
+  //       console.log(position)
+  //       this.setState({
+  //         latitude: position.coords.latitude,
+  //         longitude: position.coords.longitude,
+  //         error: null,
+  //       });
+  //     },
+  //     (error) => this.setState({ error: error.message }),
+  //     { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+  //   );
+  // }
+
+  return (
+    <View style={{flex: 1}}>
+      <MapView 
+      style={StyleSheet.absoluteFill}
+      initialRegion={props.initialRegion}>
+      {props.stores.map((store) => {
+        {/*[latitude, longitude] = store.location;*/}
+        var latitude = Number(store.location[0]);
+        var longitude = Number(store.location[1])
+        return (
+          <MapView.Marker
+            key={store.zomato_id}
+            coordinate={{latitude, longitude}}>
+            {/*change what pin does when selected and clicked*/}
+            <MapView.Callout/>
+          </MapView.Marker>
+        )
+      })}
+      </MapView>
+    </View>
+  )
 }
+
+module.exports = Map;
